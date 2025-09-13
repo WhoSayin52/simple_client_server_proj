67be2a5b0913aaffdde76a680ee3f38ec06a1dc1
Hussein Ayad <30199690-WhoSayin52@users.noreply.gitlab.com>
2025-09-13 20:00:23 +0300

    finished signup login template

---
+++ frontend/switch_form.js
@@ -0,0 +1,9 @@
+function switchLogin() {
+	document.getElementById("signup").style.display = "none";
+	document.getElementById("login").style.display = "flex";
+}
+
+function switchSignup() {
+	document.getElementById("login").style.display = "none";
+	document.getElementById("signup").style.display = "flex";
+}
