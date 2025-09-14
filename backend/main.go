48fa80451c4847a8a312bc2cd13eb477d00015d6
Hussein Ayad <30199690-WhoSayin52@users.noreply.gitlab.com>
2025-09-14 17:00:14 +0300

    Implemented basic server. Started user signup/login forms backend handlers

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	fs := http.FileServer(http.Dir("../frontend/"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/signup", signupHandler)
+
+	fmt.Println("Server running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
--- frontend/switch_form.js
+++ frontend/switch_form.js
@@ -1,9 +1,9 @@
 function switchLogin() {
-	document.getElementById("signup").style.display = "none";
-	document.getElementById("login").style.display = "flex";
+	document.getElementById("signup-form").style.display = "none";
+	document.getElementById("login-form").style.display = "flex";
 }
 
 function switchSignup() {
-	document.getElementById("login").style.display = "none";
-	document.getElementById("signup").style.display = "flex";
+	document.getElementById("login-form").style.display = "none";
+	document.getElementById("signup-form").style.display = "flex";
 }
